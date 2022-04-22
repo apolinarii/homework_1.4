c5a2215dc68c6ee31272e34b66de70c43ff45a56
apolinarii <borodesku@gmail.com>
2022-04-22 14:51:26 +0300

    first commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,34 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
-	//mmm
+    public static void main(String[] args){
+        int i = 1;
+        while (i <= 10) {
+            System.out.println(i++);
+        }
+        for (int a = 10; a >= 0; a--){
+            System.out.println(a);
+        }
+        int friday = 4;
+
+        for (friday = friday; friday <= 31; friday = friday + 7) {
+
+            if (friday < 1 && friday > 7) {
+                System.out.print("Mistake - Friday must be in the first seven days");
+            }
+            System.out.println("Today is Friday " + friday + " day of the month. It's time to submit a report");
+
+        }
+        int lastCentury = 1822;
+        int nextCentury = 2122;
+        for (int comet = 0; comet <= nextCentury; comet +=79){
+            if (comet >= lastCentury && comet <= nextCentury);{
+                System.out.println(comet);
+            }
+        }
+
     }
-}
+
+    }
+
+
